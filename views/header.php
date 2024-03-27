24a23b2b6763664e5694bd95711c953490fa695d
StayZe <mandra.antoine@gmail.com>
2024-03-27 17:46:52 +0100

    ok

--- views/header.php
+++ views/header.php
@@ -2,8 +2,9 @@
     <ul class="left-header">
         <li>Home</li>
         <li>Favoris</li>
-
     </ul>
+
+    <img src="https://i.pinimg.com/564x/a0/77/7f/a0777f53e46e0a5c72a4f7c86e3fb0f2.jpg" alt="">
 
     <ul class="right-header">
         <li>Register</li>
