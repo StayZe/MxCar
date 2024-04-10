b9363326984b6207c43a7bf2caa1e17565100746
Ethan RIEU <ethan23.rieu@gmail.com>
2024-04-10 17:01:11 +0200

    ok

--- views/account-home.php
+++ views/account-home.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MxCar | Account</title>
 </head>
+
 <body>
+    <?php require("./views/headerConnected.php") ?><br><br>
     <a href="./index.php?action=uploadCar">Upload a car</a>
 </body>
+
 </html>
