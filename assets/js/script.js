0e41517db915c34738e23658f945bc181cfc054f
StayZe <mandra.antoine@gmail.com>
2024-04-24 12:28:30 +0200

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,11 @@ function left() {
 function right() {
     const WidthSlider = document.querySelector('.slider').offsetWidth;
     document.querySelector('.slider_content_item').scrollLeft += WidthSlider;
+    if (WidthSlider.scrollLeft + slider.offsetWidth >= maxScroll) {
+        // Si nous sommes à la fin du slider, revenir au début
+        WidthSlider.scrollLeft = 0;
+    } else {
+        // Sinon, faire défiler normalement
+        sliderContent.scrollLeft += WidthSlider;
+    }
 }
